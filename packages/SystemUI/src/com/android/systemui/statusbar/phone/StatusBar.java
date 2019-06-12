0141dcf3d8eaf48d391695e23e91223a8c2ea8bc
Weijie Wang <weijiew@codeaurora.org>
2019-06-12 11:07:43 +0800

    SystemUI: Fix SystemUI failed to go to doze issue
    DozeService is started before screen turned off, which leads
    SystemUI to go to doze failed. SystemUI doesn't request turn
    on screen when receiving new message.
    
    In this case, SystemUI updates doze state after screen turned
    off, so that SystemUI can go to doze.
    
    Change-Id: Ib4386301f2f0b1ea1c349346b5d7894aeca37abf
    CRs-Fixed: 2468758

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -5476,6 +5476,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         @Override
         public void onScreenTurnedOff() {
+            updateDozing();
             mFalsingManager.onScreenOff();
             mScrimController.onScreenTurnedOff();
             mVisualizerView.setVisible(false);
@@ -6099,6 +6100,8 @@ public class StatusBar extends SystemUI implements DemoMode,
                 DozeLog.traceDozing(mContext, mDozing);
                 updateDozing();
                 updateIsKeyguard();
+            }else{
+                mDozingRequested = true;
             }
         }
 
